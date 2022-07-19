397edb81cb36e185cfac69b0eb359c2ecc7874f2
byeounghoon <kimbyeounghoon@naver.com>
2022-07-19 11:44:38 +0900

    [Solve] ✍️ BOJ16198 에너지 모으기

---
+++ Weeks/Week04/WED/BOJ16198_BH.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.swing.event.AncestorEvent;
+
+public class BOJ16198_BH {
+    static int N;
+    static ArrayList<Integer> list = new ArrayList<>();
+    static boolean[] visited;
+    static int answer = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Backtracking(list, 0);
+        System.out.println(answer);
+    }
+
+    public static void Backtracking(ArrayList<Integer> list, int sum) {
+        if (list.size() == 2) {
+            answer = Math.max(answer, sum);
+        }
+
+        for (int i = 1; i < list.size() - 1; i++) {
+            int temp = list.get(i);
+            int num = list.get(i - 1) * list.get(i + 1);
+            list.remove(i);
+            // for (int s : list) {
+            // System.out.print(s + " ");
+            // }
+            // System.out.println();
+            Backtracking(list, sum + num);
+            list.add(i, temp);
+        }
+    }
+}
